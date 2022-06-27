8e9f1206e2334a79bdabc48167d07b6fa1f3fdcf
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2022-06-27 09:49:35 +0000

    use generic dataset name

--- configs/ballseg.py
+++ configs/ballseg.py
@@ -20,7 +20,7 @@ experiment_type = [
 output_shape = (640, 640)
 
 # DeepSport Dataset
-dataset_name = "/DATA/datasets/gva/deepsport-camera-views-dataset.pickle"
+dataset_name = "camera_with_ball_visible_views.pickle"
 size_min = 14
 size_max = 37
 transforms = [
