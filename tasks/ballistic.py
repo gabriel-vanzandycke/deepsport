6e344f31f1d56c0f3ff830fbe4523d72f73e5bef
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2022-12-23 18:57:19 +0100

    wip

--- tasks/ballistic.py
+++ tasks/ballistic.py
@@ -10,7 +10,7 @@ from dataset_utilities.ds.raw_sequences_dataset import BallState
 
 
 g = 9.81 * 100 /(1000*1000) # m/s² => cm/ms²
-
+np.set_printoptions(precision=3, linewidth=120)#, suppress=True)
 
 def project_3D_to_2D(P, points3D):
     points2D_H = np.einsum('bij,jb->ib', P, points3D.H)
@@ -26,7 +26,7 @@ def compute_length2D(K, RT, points3D, length, points2D=None):
     answer = np.linalg.norm(points2D - Point2D(points2D_H) * np.sign(points2D_H[2]), axis=0)
     return answer
 
-def model_2D(weights=(1, 1), **kwargs):
+def model_2D(weights=None, v_scale=1, p_scale=1, M='minimize', **kwargs):
     weights = np.vstack(weights)
 
     class BallisticModel():
@@ -41,16 +41,6 @@ def model_2D(weights=(1, 1), **kwargs):
             t = t - self.T0
             return self.p0 + self.v0*t + self.a0*t**2/2
 
-        @staticmethod
-        def position_error(P, p_data, points3D):
-            p_pred = project_3D_to_2D(P, points3D)
-            return np.linalg.norm(p_data - p_pred, axis=0)
-
-        @staticmethod
-        def diameter_error(K, RT, d_data, points3D, points2D=None):
-            d_pred = compute_length2D(K, RT, points3D, BALL_DIAMETER, points2D=points2D)
-            return d_data - d_pred
-
         @classmethod
         def fit(cls, samples):
             T0 = samples[0].timestamp
@@ -61,22 +51,40 @@ def model_2D(weights=(1, 1), **kwargs):
             points3D = Point3D([s.ball.center for s in samples])
             p_data = project_3D_to_2D(P, points3D)
             d_data = compute_length2D(K, RT, points3D, BALL_DIAMETER, points2D=p_data)
+            p_error = None
+            d_error = None
+            p0 = samples[0].ball.center
 
-            def error(initial_conditions):
-                model = BallisticModel(initial_conditions, T0)
+            def error(initial_condition):
+                p0 = Point3D(initial_condition[0:3])/p_scale
+                v0 = Point3D(initial_condition[3:6])/v_scale
+                initial_condition = p0.x, p0.y, p0.z, v0.x, v0.y, v0.z
+                nonlocal p_error, d_error
+                model = BallisticModel(initial_condition, T0)
                 points3D = model(timestamps)
-                p_error = cls.position_error(P, p_data, points3D)
-                d_error = cls.diameter_error(K, RT, d_data, points3D, points2D=project_3D_to_2D(P, points3D))
+
+                p_pred = project_3D_to_2D(P, points3D)
+                p_error = np.linalg.norm(p_data - p_pred, axis=0)
+
+                d_pred = compute_length2D(K, RT, points3D, BALL_DIAMETER, points2D=p_pred)
+                d_error = d_data - d_pred
+
+                #return p_error + np.linalg.norm(d_error, axis=0) # to delete
                 return (np.stack([p_error, d_error])*weights).flatten()
 
             p0 = samples[0].ball.center
-            v0 = samples[1].ball.center - samples[0].ball.center
+            v0 = (samples[-1].ball.center - samples[0].ball.center)/(samples[-1].timestamp - samples[0].timestamp)
+            p0 = p0*p_scale
+            v0 = v0*v_scale
+            v0.z = (samples[1].ball.center.z - samples[0].ball.center.z)/(samples[1].timestamp - samples[0].timestamp)
             initial_guess = p0.x, p0.y, p0.z, v0.x, v0.y, v0.z
             result = scipy.optimize.least_squares(error, initial_guess, **kwargs)
             if not result.success:
                 return None
             self = cls(result['x'], T0)
-            self.error = np.hsplit(result['fun'], 2)/weights
+            #self.error = np.hsplit(result['fun'], 2)/weights
+            #print(result['fun'])
+            self.error = np.stack([p_error, d_error])
             self.samples = samples
             return self
 
@@ -84,14 +92,14 @@ def model_2D(weights=(1, 1), **kwargs):
 
 
 class SlidingWindow:
-    def __init__(self, length, step, weights=(1, 1), **kwargs):
+    def __init__(self, length, step, threshold, min_inliers_ratio=.8, min_inliers=5, weights=None, **kwargs):
         self.length = length
         self.step = step
         self.window = []
         self.Model = model_2D(weights=weights, **kwargs)
-        self.threshold = np.array([[10], [10]]) # position error, diameter error
-        self.min_inliers_ratio = .8 # min inliers ratio between first and last inliers
-        self.min_inliers = 6  # min inliers to consider the model valid
+        self.threshold = threshold#np.array([[10], [10]]) # position error, diameter error
+        self.min_inliers_ratio = min_inliers_ratio # min inliers ratio between first and last inliers
+        self.min_inliers = min_inliers  # min inliers to consider the model valid
         self.popped = 0
     def pop(self, count, callback=None):
         for i in range(count):
@@ -106,27 +114,36 @@ class SlidingWindow:
         for _ in range(self.length - self.step):
             self.window.append(next(gen))
 
+        model = None
         while True:
+            print("")
             #while len(self.window) < self.length:
             for _ in range(self.step):
                 self.window.append(next(gen))
-            print(" "*self.popped, "".join(["*" if s.ball.state == BallState.FLYING else "." for s in self.window]))
 
-            model = self.Model.fit(self.window)
-            if model is None:
+            new_model = self.Model.fit(self.window)
+            print(" "*self.popped + "".join(["*" if s.ball.state == BallState.FLYING else "." for s in self.window]))
+            if new_model is None:
+                print("")
+                callback = lambda i, s: setattr(s.ball, "model", model if model.inliers[0] <= i <= model.inliers[-1] and model is not None else None)
                 yield from self.pop(len(self.window) - self.length + self.step)
+                model = None
                 continue
 
-            inliers = np.where(np.all(model.error < self.threshold, axis=0))[0]
-            inliers_ratio = (inliers[-1] - inliers[0] + 1)/len(inliers)
+            inliers = np.where(new_model.error[0] < self.threshold)[0]
+            print(" "*self.popped + "".join(["-" if i in inliers else " " for i in range(len(self.window))]))
+            inliers_ratio = len(inliers)/(inliers[-1] - inliers[0] + 1)
             if len(inliers) < self.min_inliers or inliers_ratio < self.min_inliers_ratio:
+                callback = lambda i, s: setattr(s.ball, "model", model if model.inliers[0] <= i <= model.inliers[-1] and model is not None else None)
                 yield from self.pop(len(self.window) - self.length + self.step)
+                model = None
                 continue
 
-            inliers = range(inliers[0], inliers[-1] + 1)
+            model = new_model
+            model.inliers = inliers
             if len(self.window) - inliers[-1] > self.length:
-                callback = lambda i, s: setattr(s.ball, "center_new", model(s.timestamp) if i in inliers else None)
-                yield from self.pop(inliers[-1], callback)
+                callback = lambda i, s: setattr(s.ball, "model", model if model.inliers[0] <= i <= model.inliers[-1] and model is not None else None)
+                yield from self.pop(inliers[-1]+1, callback)
                 continue
 
 
@@ -243,14 +260,13 @@ class MSE2DOptimizer(Optimizer):
         def error(params):
             model = BallisticModel(*params, T0)
             error = self.compute_error(P, K, RT, timestamps, p_data, d_data, model)
-            return error
+            return np.mean(error)
         p0 = samples[0].ball.center
         v0 = samples[1].ball.center - samples[0].ball.center
         initial_guess = p0.x, p0.y, p0.z, v0.x, v0.y, v0.z
-        result = scipy.optimize.least_squares(error, initial_guess)
-        print(result)
+        result = scipy.optimize.minimize(error, initial_guess)
+        print(result.message)
         self.model = BallisticModel(*result['x'], T0)
-        raise
     def inliers(self, samples):
         error = self.error(samples)
         return np.where(error < self.threshold)[0]
@@ -265,6 +281,7 @@ class Iterative2DOptimizer(MSE2DOptimizer):
     def fit(self, samples, T0):
         super().fit(samples, T0)
         inliers = self.inliers(samples)
+        print(inliers)
         if len(inliers) < self.min_inliers:
             self.model = None
             return
