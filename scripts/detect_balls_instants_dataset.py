2360c8681d944beadca2db7bb7fd2860471d7937
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2023-03-13 11:37:05 +0100

    finilize script to detect balls in deepsport instants dataset

--- scripts/detect_balls_instants_dataset.py
+++ scripts/detect_balls_instants_dataset.py
@@ -1,47 +1,80 @@
+#!/usr/bin/env python
 import argparse
 import os
+import multiprocessing
 
-import dotenv
+from collections import defaultdict
+
+print("Python Executable:", os.sys.executable)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+from dotenv import load_dotenv, find_dotenv
 from tqdm.auto import tqdm
 
+from pyconfyg import Confyg
 from mlworkflow import TransformedDataset, FilteredDataset
 from deepsport_utilities import import_dataset
 from deepsport_utilities.ds.instants_dataset import InstantsDataset, DownloadFlags, CropBlockDividable
 
 from tasks.detection import DetectBalls, PIFBALL_THRESHOLD, BALLSEG_THRESHOLD
 
-dotenv.load_dotenv()
-
-default_shapes = ["2054,2456", "1458,1936", "2360,1992", "1234,1624", "2456,1520", "2456,1800", "1752,2336"]
+load_dotenv(find_dotenv(usecwd=True))
 
 parser = argparse.ArgumentParser(description="""
 Detect ball in Basketball Instants Dataset.""")
-parser.add_argument("name", help="Detector name (sets ball 'origin' field)")
-parser.add_argument("shapes", nargs="*", default=default_shapes)
-parser.add_argument("--dataset-folder", default=None, help="Basketball Instants Dataset folder")
+parser.add_argument("detector", help="Detector name (sets ball 'origin' field)")
+parser.add_argument("--workers", type=int, default=2)
 parser.add_argument("--k", default=4, help="Maximum number of detections to consider")
 args = parser.parse_args()
 
-dataset_folder = args.dataset_folder or os.path.join(os.environ['LOCAL_STORAGE'], "basketball-instants-dataset")
 dataset_config = {
     "download_flags": DownloadFlags.WITH_IMAGE | DownloadFlags.WITH_FOLLOWING_IMAGE | DownloadFlags.WITH_CALIB_FILE,
-    "dataset_folder": dataset_folder  # informs dataset items of raw files location
+    "dataset_folder": os.path.join(os.environ['LOCAL_STORAGE'], "basketball-instants-dataset")
 }
-
 database_file = os.path.join(dataset_config['dataset_folder'], "basketball-instants-dataset.json")
-threshold, experiment_id = {
-    "pifball": (PIFBALL_THRESHOLD, "20220830_142415.448672"),
-    "ballseg": (BALLSEG_THRESHOLD, "20220829_144032.694734"),
-}[args.name]
-config = os.path.join(os.environ['RESULTS_FOLDER'], args.name, experiment_id, 'config_inference.py')
 
-for shape in tqdm(args.shapes):
-    ids = import_dataset(InstantsDataset, database_file, **dataset_config)
-    ids = FilteredDataset(ids, lambda k,v: v.images[0].shape[0:2] == eval(shape))
-    ids = TransformedDataset(ids, [CropBlockDividable()])
+experiment_ids = {
+    "ballseg": ["20230221_210804.236154", "20230221_111901.374475", "20230217_184505.715658", "20230217_140646.069593", "20230221_165826.810944", "20230220_132352.389741", "20230217_184403.495582", "20230217_140644.619392"],
+    "pifball": ["20230221_202338.343195", "20230220_202551.530667", "20230220_135417.554986", "20230217_150524.178161", "20230222_035443.776450", "20230221_210246.162130", "20230221_111435.426128", "20230217_150541.149283"],
+}[args.detector]
 
-    detector = DetectBalls(dataset_folder, args.name, config, args.k, threshold)
+threshold = {
+    "pifball": PIFBALL_THRESHOLD/2,
+    "ballseg": BALLSEG_THRESHOLD/2,
+}[args.detector]
 
-    for instant_key in tqdm(ids.yield_keys()):
-        instant = ids.query_item(instant_key)
+ids = import_dataset(InstantsDataset, database_file, **dataset_config)
+
+def process(ds, config):
+    ds = TransformedDataset(ds, [CropBlockDividable()])
+    detector = DetectBalls(ids.dataset_folder, name=args.detector, config=config, k=args.k, detection_threshold=threshold)
+    for instant_key in tqdm(ds.yield_keys(), leave=False):
+        instant = ds.query_item(instant_key)
         detector(instant_key, instant)
+
+def process_model(experiment_id):
+    config = os.path.join(os.environ['RESULTS_FOLDER'], args.detector, experiment_id, 'config.py')
+    arena_labels = Confyg(config).dict['testing_arena_labels']
+    ds = FilteredDataset(ids, lambda k: k.arena_label in arena_labels)
+
+    keys_per_shape = defaultdict(list)
+    for key in tqdm(ds.keys, leave=False, desc=f"Collecting shapes for {experiment_id}"):
+        keys_per_shape[ds.query_item(key).images[0].shape].append(key)
+
+    for shape, keys in tqdm(keys_per_shape.items(), leave=False, desc=f"Processing shapes of {experiment_id}"):
+        print(f"Processing {len(keys)} items with shape {shape} with {experiment_id}", flush=True)
+        process(FilteredDataset(ds, lambda k: k in keys), config)
+        print(f"Done processing {len(keys)} items with shape {shape} with {experiment_id}", flush=True)
+
+
+with multiprocessing.Pool(args.workers) as pool:
+    results = pool.map_async(process_model, experiment_ids,
+        callback=lambda _: print("Done", flush=True),
+        error_callback=lambda e: print(e, flush=True)
+    )
+    results.wait()
+
+print("Done with all arenas and shapes")
+
+
+
