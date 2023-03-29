8315934a0c591a6b7af3bba3cf4071e54ac36b98
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2023-03-29 17:30:25 +0200

    six channels tensorflow model with duplicated first layer

--- models/tensorflow.py
+++ models/tensorflow.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 from experimentator.tf2_chunk_processors import ChunkProcessor
 
@@ -10,12 +11,39 @@ class TensorflowBackbone(ChunkProcessor):
         self.include_top = include_top
         self.args = args
         self.kwargs = kwargs
+    def init_network(self, input_shape):
+        model_str = f"tf.keras.applications.{self.model_name}"
+        print(f"Initializing '{model_str}' with {input_shape} input")
+        self.network = eval(model_str)(input_shape=input_shape, include_top=self.include_top, *self.args, **self.kwargs) # pylint: disable=eval-used
+        #print(self.network.summary())
+
     def __call__(self, chunk):
         if getattr(self, "network", None) is None:
-            input_shape = chunk["batch_input"].get_shape()[1:4]
-            model_str = f"tf.keras.applications.{self.model_name}"
-            print(f"Initializing '{model_str}' with input", chunk["batch_input"])
-            self.network = eval(model_str)(input_shape=input_shape, include_top=self.include_top, *self.args, **self.kwargs) # pylint: disable=eval-used
-            #print(self.network.summary())
+            self.init_network(chunk["batch_input"].get_shape()[1:4])
         chunk["batch_logits"] = self.network(chunk["batch_input"], training=True)
 
+
+class SixChannelsTensorflowBackbone(TensorflowBackbone):
+    def init_network(self, input_shape):
+        H, W, C = input_shape
+        super().init_network((H, W, 3))
+        if C != 3:
+            assert C == 6, f"Expected 3 or 6 channels, got {C}"
+            # Retrieve first layer specifications
+            layer = self.network.layers[1]
+            weights, biaises = layer.weights
+            config = layer.get_config()
+
+            # Duplicate first layer channels
+            weights = tf.concat([weights]*2, 2)
+
+            # Recreate first layer
+            layer = layer.__class__.from_config(config)
+            layer.build(input_shape[1:])
+            layer.set_weights((np.array(weights), np.array(biaises)))
+
+            # Recreate model
+            self.network = tf.keras.Sequential([
+                layer,
+                *self.network.layers[2:]
+            ], self.network.name)
