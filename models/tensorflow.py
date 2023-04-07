2518ef027d8c04eadd002b529c69db4413683083
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2023-04-07 16:01:55 +0200

    tensorflow 6 layers input models (+ skip connections of input tensor to each convolutional features blocks)

--- models/tensorflow.py
+++ models/tensorflow.py
@@ -29,12 +29,14 @@ class SixChannelsTensorflowBackbone(TensorflowBackbone):
         super().init_network((H, W, 3))
         if C != 3:
             assert C == 6, f"Expected 3 or 6 channels, got {C}"
+            print(f"Re-initializing network with {C} input channels")
+
             # Retrieve first layer specifications
             layer = self.network.layers[1]
             weights, biaises = layer.weights
             config = layer.get_config()
 
-            # Duplicate first layer channels
+            # Dupldicate first layer channels
             weights = tf.concat([weights]*2, 2)
 
             # Recreate first layer
@@ -47,3 +49,44 @@ class SixChannelsTensorflowBackbone(TensorflowBackbone):
                 layer,
                 *self.network.layers[2:]
             ], self.network.name)
+
+class SkipConnectionCroppedInputsModel(tf.keras.Model):
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+
+    def build(self, input_shape):
+        C = input_shape[-1]
+        self._layers = []
+        first_weights = None
+        for layer in self.model.layers[1:]:
+            self._layers.append(layer.__class__.from_config(layer.get_config()))
+            if isinstance(self._layers[-1], tf.keras.layers.Conv2D):
+                weights, biaises = layer.weights
+                _, h, w, c = layer.input_shape
+                if first_weights is None:
+                    first_weights = weights
+                else:
+                    weights = tf.concat([weights, weights[:,:,0:C]], axis=2)
+                    c = c+C
+
+                self._layers[-1].build((h, w, c))
+                self._layers[-1].set_weights((np.array(weights), np.array(biaises)))
+
+    def call(self, input_tensor):
+        _, H, W, _ = input_tensor.shape
+        x = input_tensor
+        for i, layer in enumerate(self._layers):
+            if isinstance(layer, tf.keras.layers.Conv2D) and i != 0: # Skip first layer as well
+                _, h, w, c = x.shape
+                x_slice = slice((W-w)//2, (W-w)//2+w, None)
+                y_slice = slice((H-h)//2, (H-h)//2+h, None)
+                x = layer(tf.concat([x, input_tensor[:, y_slice, x_slice]], axis=-1))
+            else:
+                x = layer(x)
+        return x
+
+
+class SkipConnectionCroppedInputsModelSixCannels(SixChannelsTensorflowBackbone):
+    def init_network(self, input_shape):
+        return SkipConnectionCroppedInputsModel(super().init_network(input_shape))
