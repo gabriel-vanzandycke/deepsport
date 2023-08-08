d79b7df71e6dabbe75f2076fb8a43c1ab972228a
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2023-08-08 11:54:37 +0200

    ballheight

--- configs/ballheight.py
+++ configs/ballheight.py
@@ -20,58 +20,117 @@ experiment_type = [
 batch_size = 16
 
 # Dataset parameters
-side_length = 224
-output_shape = (side_length, side_length)
+scale = 1
+side_length = 512
+output_shape = (int(side_length*scale), int(side_length*scale))
 
 estimate_diameter = False
 estimate_presence = False
-full_dataset = False
 with_diff = False
 draw_vertical_cues = False
+public_dataset = True
+correct_distortion = False
 
 # DeepSport Dataset
-dataset_name = f"ballsize_dataset_640_{'with' if estimate_presence else 'no'}_detections.pickle"
-scale = .5
-size_min = 14*scale
-size_max = 40*scale
+dataset_name = {
+    (True, False): "ballsize_dataset_1000_with_detections_from_model_trained_on_full_dataset.pickle",
+    (True, True): "ballsize_dataset_1000_with_detections_from_model_trained_on_small_dataset.pickle",
+    (False, True): "ballsize_dataset_1000_no_detections.pickle",
+    (False, False): "ballsize_dataset_1000_no_detections.pickle",
+}[(estimate_presence, public_dataset)]
+
 max_shift = 0
-transforms = [
-    deepsport_utilities.ds.instants_dataset.BallViewRandomCropperTransform(
-        output_shape=output_shape,
-        size_min=size_min,
-        size_max=size_max,
-        margin=side_length//2-max_shift,
-        padding=int(side_length/scale),
-        on_ball=True,
-    ),
-    deepsport_utilities.transforms.DataExtractorTransform(
-        deepsport_utilities.ds.instants_dataset.views_transforms.AddImageFactory(),
-        deepsport_utilities.ds.instants_dataset.views_transforms.AddNextImageFactory() if with_diff else None,
-        deepsport_utilities.ds.instants_dataset.views_transforms.AddBallSizeFactory(origins=['annotation', 'interpolation', 'ballseg']) if estimate_diameter else None,
-        deepsport_utilities.ds.instants_dataset.views_transforms.AddBallHeightFactory(origins=['annotation', 'interpolation', 'ballseg']),
-        deepsport_utilities.ds.instants_dataset.views_transforms.AddBallFactory(),
-        deepsport_utilities.ds.instants_dataset.views_transforms.AddCalibFactory(),
-        deepsport_utilities.ds.instants_dataset.views_transforms.AddIsBallTargetFactory() if estimate_presence else None,
-    )
+
+data_extractor_transform = deepsport_utilities.transforms.DataExtractorTransform(
+    deepsport_utilities.ds.instants_dataset.views_transforms.AddImageFactory(),
+    deepsport_utilities.ds.instants_dataset.views_transforms.AddNextImageFactory() if with_diff else None,
+    deepsport_utilities.ds.instants_dataset.views_transforms.AddBallSizeFactory(origins=['annotation', 'interpolation', 'ballseg']) if estimate_diameter else None,
+    deepsport_utilities.ds.instants_dataset.views_transforms.AddBallHeightFactory(origins=['annotation', 'interpolation', 'ballseg']),
+    deepsport_utilities.ds.instants_dataset.views_transforms.AddBallFactory(),
+    deepsport_utilities.ds.instants_dataset.views_transforms.AddCalibFactory(),
+    deepsport_utilities.ds.instants_dataset.views_transforms.AddIsBallTargetFactory() if estimate_presence else None,
+)
+
+fixed_scale_cropper_transform = deepsport_utilities.ds.instants_dataset.BallViewRandomCropperTransform(
+    output_shape=output_shape,
+    scale_min=1*scale,
+    scale_max=1*scale,
+    margin=int(side_length*scale)//2-max_shift,
+    padding=int(side_length*scale),
+    on_ball=True,
+    rectify=correct_distortion,
+)
+
+random_scale_cropper_transform = deepsport_utilities.ds.instants_dataset.BallViewRandomCropperTransform(
+    output_shape=output_shape,
+    scale_min=0.75*scale,
+    scale_max=1.25*scale,
+    margin=int(side_length*scale)//2-max_shift,
+    padding=int(side_length*scale),
+    on_ball=True,
+    rectify=correct_distortion,
+)
+
+random_size_cropper_transform = deepsport_utilities.ds.instants_dataset.BallViewRandomCropperTransform(
+    output_shape=output_shape,
+    size_min=14*scale,
+    size_max=40*scale,
+    margin=side_length//2-max_shift,
+    padding=side_length,
+    on_ball=True,
+    rectify=correct_distortion,
+)
+
+dataset = experimentator.CachedPickledDataset(find(dataset_name))
+dataset = mlwf.FilteredDataset(dataset, lambda k: bool(isinstance(k[0], deepsport_utilities.ds.instants_dataset.InstantKey)))
+dataset = mlwf.FilteredDataset(dataset, lambda k,v: estimate_presence or v.ball.origin in ['annotation', 'interpolation'])
+evaluation_dataset_name = "ballistic_ball_views_512.pickle" # "views_from_instant_sequences_dataset_smoothed_ball_annotation.pickle"
+evaluation_dataset = experimentator.CachedPickledDataset(find(evaluation_dataset_name, verbose=True))
+
+globals().update(locals()) # required to use locals() in lambdas
+
+repetitions = 1
+additional_keys_usage = 'testing2' if public_dataset else 'training'
+dataset_splitter = deepsport_utilities.ds.instants_dataset.DeepSportDatasetSplitter(
+    additional_keys_usage=additional_keys_usage,
+    validation_pc=15,
+    repetitions={'testing': repetitions})
+
+fixed_scale_subsets = dataset_splitter(mlwf.TransformedDataset(dataset, [fixed_scale_cropper_transform, data_extractor_transform]))
+random_scale_subsets = dataset_splitter(mlwf.TransformedDataset(dataset, [random_scale_cropper_transform, data_extractor_transform]))
+random_size_subsets = dataset_splitter(mlwf.TransformedDataset(dataset, [random_size_cropper_transform, data_extractor_transform]))
+
+balancer_str = None
+balancer = {
+    'two': lambda k,v: 0 if v['ball'].center.z < -200 else 1,
+    'None': None,
+    None: None
+}[balancer_str]
+
+classes = {
+    'two': [0, 1],
+    'None': None,
+    None: None
+}[balancer_str]
+
+
+if balancer is not None:
+    random_scale_subsets[0] = experimentator.BalancedSubset.convert(random_scale_subsets[0], balancer, classes)
+
+random_size_subsets[2].name = "legacy_testing"
+random_size_subsets[3].name = "legacy_testing2"
+subsets = [
+    random_scale_subsets[0],
+    fixed_scale_subsets[1],
+    fixed_scale_subsets[2],
+    fixed_scale_subsets[3],
+    random_size_subsets[2],
+    random_size_subsets[3],
+    experimentator.Subset("3d_testset",        experimentator.SubsetType.EVAL, mlwf.TransformedDataset(evaluation_dataset, [fixed_scale_cropper_transform, data_extractor_transform]), repetitions=repetitions),
+    experimentator.Subset("legacy_3d_testset", experimentator.SubsetType.EVAL, mlwf.TransformedDataset(evaluation_dataset, [random_size_cropper_transform, data_extractor_transform]), repetitions=repetitions),
 ]
 
-dataset_splitter_str = 'deepsport'
-validation_pc = 15
-testing_arena_labels = []
-dataset = experimentator.CachedPickledDataset(find(dataset_name))
-dataset = mlwf.FilteredDataset(dataset, lambda k: full_dataset or bool(isinstance(k[0], deepsport_utilities.ds.instants_dataset.InstantKey) and "KS-FR-" in k.arena_label))
-dataset = mlwf.FilteredDataset(dataset, lambda k,v: estimate_presence or v.ball.origin in ['annotation', 'interpolation'])
-globals().update(locals()) # required to use locals() in lambdas
-dataset = mlwf.TransformedDataset(dataset, transforms)
 
-fold = 0
-
-dataset_splitter = {
-    "deepsport": deepsport_utilities.ds.instants_dataset.DeepSportDatasetSplitter(additional_keys_usage='training' if full_dataset else 'skip', validation_pc=validation_pc),
-    "arenas_specific": deepsport_utilities.ds.instants_dataset.dataset_splitters.TestingArenaLabelsDatasetSplitter(testing_arena_labels, validation_pc=validation_pc),
-}[dataset_splitter_str]
-subsets = dataset_splitter(dataset, fold)
-testing_arena_labels = dataset_splitter.testing_arena_labels
 
 callbacks = [
     experimentator.AverageMetrics([".*loss"]),
@@ -90,6 +149,8 @@ callbacks = [
     experimentator.wandb_experiment.LogStateWandB("validation_MAPE", False),
 ]
 
+backbone = "vgg16.VGG16"
+
 alpha = 0.5 if estimate_presence else 0
 beta = 0.5 if estimate_diameter else 0
 globals().update(locals()) # required to use locals() in lambdas
@@ -99,7 +160,7 @@ chunk_processors = [
     models.other.GammaAugmentation("batch_input_image"),
     lambda chunk: chunk.update({"batch_input": chunk["batch_input_image"] if not with_diff else tf.concat((chunk["batch_input_image"], chunk["batch_input_diff"]), axis=3)}),
     experimentator.tf2_chunk_processors.Normalize(tensor_names=["batch_input"]),
-    models.tensorflow.SixChannelsTensorflowBackbone("vgg16.VGG16", include_top=False),
+    models.tensorflow.TensorflowBackbone(backbone, include_top=False),
     models.other.LeNetHead(output_features=3),
     tasks.ballheight.HeightEstimationNamedOutputs(),
     models.other.BinaryCrossEntropyLoss(y_true="batch_is_ball", y_pred="predicted_is_ball", name="classification") if estimate_presence else None,
